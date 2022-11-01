270fd896bcda0ae881141eebb1ee27c27e107afd
Dhanalakshmi <dhanalakshmi.m@ideas2it.com>
2022-11-01 14:40:06 +0530

    changed to user_order

--- src/main/java/com/ideas2it/groceryshop/model/Order.java
+++ src/main/java/com/ideas2it/groceryshop/model/Order.java
@@ -1,7 +1,7 @@
 package com.ideas2it.groceryshop.model;
 
 import javax.persistence.*;
-import java.sql.Date;
+import java.util.Date;
 import java.util.List;
 import com.ideas2it.groceryshop.model.OrderDetails;
 import lombok.AllArgsConstructor;
@@ -10,15 +10,14 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@Table(name = "order")
+@Table(name = "user_order")
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class Order {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     @Column(name = "ordered_date")
     private Date orderedDate;
@@ -29,5 +28,8 @@ public class Order {
     @OneToMany
     @JoinColumn(name = "order_id")
     private List<OrderDetails> orderDetails;
+    @OneToOne
+    @JoinColumn(name = "order_id" )
+    private OrderDelivery orderDelivery;
 
 }
